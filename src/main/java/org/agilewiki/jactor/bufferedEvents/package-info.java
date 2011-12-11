de65eb9e912a40c09bf381515d2233f262bfc578
Bill LaForge <laforge49@gmail.com>
2011-12-11 08:34:09 +0530

    New package-info for bufferedEvents.

---
+++ src/main/java/org/agilewiki/jactor/bufferedEvents/package-info.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2011 Bill La Forge
+ *
+ * This file is part of AgileWiki and is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License (LGPL) as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * or navigate to the following url http://www.gnu.org/licenses/lgpl-2.1.txt
+ *
+ * Note however that only Scala, Java and JavaScript files are being covered by LGPL.
+ * All other files are covered by the Common Public License (CPL).
+ * A copy of this license is also included and can be
+ * found as well at http://www.opensource.org/licenses/cpl1.0.txt
+ */
+
+/**
+ * <p>
+ * The bufferedEvents package supports the dispatching of one-way messages (events),
+ * but it buffers events and sends them only when there are no other incoming events
+ * to process for a significant improvement in throughput.
+ * </p>
+ * <p>
+ * A BufferedEventsDestination receives lists of events from objects operating
+ * on a different thread.
+ * </p>
+ * <p>
+ * A BufferedEventsQueue receives buffered events, queues them,
+ * and then processes them on another thread.
+ * </p>
+ * <p>
+ * JABufferedEventsQueue implements BufferedEventsQueue.
+ * </p>
+*/
+package org.agilewiki.jactor.bufferedEvents;
