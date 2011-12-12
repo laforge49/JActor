2fa3990528ed6d0e484d6409273e300d0a86cff5
Bill LaForge <laforge49@gmail.com>
2011-12-12 10:12:32 +0530

    Completed Driver for dualEchoTiming.

--- src/test/java/org/agilewiki/jactor/events/dualEchoTiming/Driver.java
+++ src/test/java/org/agilewiki/jactor/events/dualEchoTiming/Driver.java
@@ -9,6 +9,8 @@ public final class Driver extends JAEventActor<Object> {
 
     private Sender sender1;
     private Sender sender2;
+    private EventDestination<Object> source;
+    private int pending;
 
     public Driver(ThreadManager threadManager, int c) {
         super(threadManager);
@@ -18,7 +20,15 @@ public final class Driver extends JAEventActor<Object> {
 
     @Override
     protected void processEvent(Object event) {
-        EventDestination<Object> destination = (EventDestination<Object>) event;
-        send(destination, this);
+        if (!(event instanceof Sender)) {
+            source = (EventDestination<Object>) event;
+            pending = 2;
+            send(sender1, this);
+            send(sender2, this);
+        } else {
+            pending -= 1;
+            if (pending == 0)
+                send(source, this);
+        }
     }
 }
