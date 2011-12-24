4cf585e9586b8b33fb2075d88dc2cb603c9a9ca0
Bill LaForge <laforge49@gmail.com>
2011-12-24 19:52:42 +0530

    Interator timing tests.

---
+++ src/test/java/org/agilewiki/jactor/apc/iterator/AsyncTimingTest.java
@@ -0,0 +1,48 @@
+package org.agilewiki.jactor.apc.iterator;
+
+import junit.framework.TestCase;
+import org.agilewiki.jactor.apc.JAIterator;
+import org.agilewiki.jactor.apc.ResponseProcessor;
+
+public class AsyncTimingTest extends TestCase {
+    public void testSync() throws Exception {
+        final long c = 1L;
+
+        //final long c = 1000000000L;
+        //iterations per sec = 143410296
+        //each iteration takes 6.973 nanoseconds
+
+        final long t0 = System.currentTimeMillis();
+
+        final ResponseProcessor done = new ResponseProcessor() {
+            @Override
+            public void process(Object unwrappedResponse) throws Exception {
+                final long t1 = System.currentTimeMillis();
+                long d = t1 - t0;
+                if (d > 0) System.out.println("iterations per sec = " + c * 1000L / d);
+                System.out.println("each iteration takes " + d * 1000000. / c + " nanoseconds");
+            }
+        };
+
+        class It extends JAIterator {
+            long i = 0;
+            ResponseProcessor rp;
+
+            It(ResponseProcessor fin) throws Exception {
+                super(fin);
+            }
+
+            @Override
+            protected void process(ResponseProcessor rp) throws Exception {
+                this.rp = rp;
+            }
+        }
+
+        final It it = new It(done);
+        while (it.i < c) {
+            it.i += 1;
+            it.rp.process(null);
+        }
+        it.rp.process(this);
+    }
+}
---
+++ src/test/java/org/agilewiki/jactor/apc/iterator/SyncTimingTest.java
@@ -0,0 +1,38 @@
+package org.agilewiki.jactor.apc.iterator;
+
+import junit.framework.TestCase;
+import org.agilewiki.jactor.apc.JAIterator;
+import org.agilewiki.jactor.apc.ResponseProcessor;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
+public class SyncTimingTest extends TestCase {
+    public void testSync() throws Exception {
+        final long c = 1L;
+
+        //final long c = 1000000000L;
+        //iterations per sec = 218962119
+        //each iteration takes 4.567 nanoseconds
+
+        final long t0 = System.currentTimeMillis();
+        ResponseProcessor done = new ResponseProcessor() {
+            @Override
+            public void process(Object unwrappedResponse) throws Exception {
+                final long t1 = System.currentTimeMillis();
+                long d = t1 - t0;
+                if (d > 0) System.out.println("iterations per sec = "+c*1000L/d);
+                System.out.println("each iteration takes "+d*1000000./c+" nanoseconds");
+            }
+        };
+        
+        new JAIterator(done) {
+            long i = 0;
+            @Override
+            protected void process(ResponseProcessor rp) throws Exception {
+                i += 1;
+                if (i < c) rp.process(null);
+                else rp.process(this);
+            }
+        };
+    }
+}
