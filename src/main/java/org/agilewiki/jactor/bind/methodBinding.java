be7b824d8e03b7fb2423c418076f67a65b6577f8
Bill LaForge <laforge49@gmail.com>
2012-01-08 12:47:29 +0530

    bind test is working

--- src/main/java/org/agilewiki/jactor/bind/methodBinding.java
+++ src/main/java/org/agilewiki/jactor/bind/methodBinding.java
@@ -1,33 +1,11 @@
 package org.agilewiki.jactor.bind;
 
-import org.agilewiki.jactor.ResponseProcessor;
-import org.agilewiki.jactor.lpc.RequestSource;
-
 /**
- * Binds a request class to a method.
+ * Created by IntelliJ IDEA.
+ * User: sony
+ * Date: 1/8/12
+ * Time: 12:47 PM
+ * To change this template use File | Settings | File Templates.
  */
-abstract public class MethodBinding extends Binding {
-    /**
-     * Process an incoming request.
-     *
-     * @param requestSource The originator of the request.
-     * @param request       The request to be sent.
-     * @param rp            The request processor.
-     * @throws Exception Any uncaught exceptions raised while processing the request.
-     */
-    @Override
-    final public void acceptRequest(RequestSource requestSource, Object request, ResponseProcessor rp)
-            throws Exception {
-        internals.acceptRequest(requestSource, request, rp);
-    }
-
-    /**
-     * The application method for processing requests sent to the actor.
-     *
-     * @param request           A request.
-     * @param responseProcessor The response processor.
-     * @throws Exception Any uncaught exceptions raised while processing the request.
-     */
-    abstract protected void processRequest(Object request, ResponseProcessor responseProcessor)
-            throws Exception;
+public class methodBinding {
 }
