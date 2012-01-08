cb18b485f19a7786c723cd054837542add2091ff
Bill LaForge <laforge49@gmail.com>
2012-01-08 17:39:17 +0530

    Folded parent test into MethodBindingTest

--- src/test/java/org/agilewiki/jactor/bind/MethodBindingTest.java
+++ src/test/java/org/agilewiki/jactor/bind/MethodBindingTest.java
@@ -3,8 +3,21 @@ package org.agilewiki.jactor.bind;
 import junit.framework.TestCase;
 import org.agilewiki.jactor.*;
 
-public class ParentTest extends TestCase {
-    public void test() {
+public class MethodBindingTest extends TestCase {
+    public void test1() {
+        MailboxFactory mailboxFactory = JAMailboxFactory.newMailboxFactory(1);
+        try {
+            Actor a = new A(mailboxFactory.createMailbox());
+            JAFuture future = new JAFuture();
+            System.err.println(future.send(a, new Hi()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            mailboxFactory.close();
+        }
+    }
+
+    public void test2() {
         MailboxFactory mailboxFactory = JAMailboxFactory.newMailboxFactory(1);
         try {
             Actor a = new A(mailboxFactory.createMailbox());
