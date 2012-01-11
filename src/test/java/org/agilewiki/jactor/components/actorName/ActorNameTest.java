675adcaa851ccb20db25a36417382db04e5c4081
Bill LaForge <laforge49@gmail.com>
2012-01-11 12:09:21 +0530

    ActorNameTest works.

---
+++ src/test/java/org/agilewiki/jactor/components/actorName/ActorNameTest.java
@@ -0,0 +1,29 @@
+package org.agilewiki.jactor.components.actorName;
+
+import junit.framework.TestCase;
+import org.agilewiki.jactor.JAFuture;
+import org.agilewiki.jactor.JAMailboxFactory;
+import org.agilewiki.jactor.MailboxFactory;
+import org.agilewiki.jactor.components.ActorName;
+import org.agilewiki.jactor.components.GetActorName;
+import org.agilewiki.jactor.components.SetActorName;
+import org.agilewiki.jactor.composite.Include;
+import org.agilewiki.jactor.composite.JCActor;
+
+public class ActorNameTest extends TestCase {
+    public void test() {
+        MailboxFactory mailboxFactory = JAMailboxFactory.newMailboxFactory(1);
+        try {
+            JAFuture future = new JAFuture();
+            JCActor a = new JCActor(mailboxFactory.createMailbox());
+            future.send(a, new Include(ActorName.class));
+            System.err.println(future.send(a, new GetActorName()));
+            future.send(a, new SetActorName("foo"));
+            System.err.println(future.send(a, new GetActorName()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            mailboxFactory.close();
+        }
+    }
+}
