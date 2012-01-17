f5c0808aefe1cb3ad57da1c2090ba98bc8775b4a
Bill LaForge <laforge49@gmail.com>
2012-01-17 18:14:37 +0530

    bind.EventTest works.

---
+++ src/test/java/org/agilewiki/jactor/bind/EventTest.java
@@ -0,0 +1,55 @@
+package org.agilewiki.jactor.bind;
+
+import junit.framework.TestCase;
+import org.agilewiki.jactor.*;
+import org.agilewiki.jactor.lpc.RequestSource;
+
+public class EventTest extends TestCase {
+    public void test() {
+        MailboxFactory mailboxFactory = JAMailboxFactory.newMailboxFactory(1);
+        try {
+            Actor a = new A(mailboxFactory.createMailbox());
+            JAEvent event = new JAEvent();
+            JAFuture future = new JAFuture();
+            Hi hi = new Hi();
+            event.send(a, hi);
+            future.send(a, hi);
+            Ho ho = new Ho();
+            event.send(a, ho);
+            future.send(a, ho);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            mailboxFactory.close();
+        }
+    }
+
+    class Hi {
+    }
+
+    class Ho {
+    }
+
+    class A extends JBActor {
+
+        A(Mailbox mailbox) {
+            super(mailbox);
+
+            bind(Hi.class.getName(), new MethodBinding() {
+                protected void processRequest(Object request, ResponseProcessor rp)
+                        throws Exception {
+                    System.err.println("A got request");
+                    rp.process("Hello world!");
+                }
+            });
+
+            bind(Ho.class.getName(), new SyncBinding() {
+                @Override
+                public void acceptRequest(RequestSource requestSource, Object request, ResponseProcessor rp) throws Exception {
+                    System.err.println("A got request");
+                    rp.process("Hello world!");
+                }
+            });
+        }
+    }
+}
