2fb10ac3c8b4accf5da79ff01375c3cd9cbaadbc
Bill LaForge <laforge49@gmail.com>
2012-01-22 09:10:10 +0530

    javadoc added for pubsub package.

---
+++ src/main/java/org/agilewiki/jactor/components/pubsub/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * <p>
+ *     One possible implementat of publish/subscribe.
+ * </p>
+ * <p>
+ *     This package uses 2-way messaging, with the advantage that a publish
+ *     operation returns no result until the message has been processed by
+ *     all subscribers.
+ * </p>
+ * <p>
+ *     There is no defined order for publishing requests to subscribers.
+ *     And like JAParallel, requests are published in parallel.
+ * </p>
+ */
+package org.agilewiki.jactor.components.pubsub;
