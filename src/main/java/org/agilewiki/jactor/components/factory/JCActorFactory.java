3eb60a45a4ae89d96514a57aa2aa23ce661de1a9
Bill LaForge <laforge49@gmail.com>
2012-03-26 15:29:03 +0530

    new JCActorFactory

---
+++ src/main/java/org/agilewiki/jactor/components/factory/JCActorFactory.java
@@ -0,0 +1,44 @@
+package org.agilewiki.jactor.components.factory;
+
+import org.agilewiki.jactor.Actor;
+import org.agilewiki.jactor.Mailbox;
+import org.agilewiki.jactor.components.Include;
+import org.agilewiki.jactor.components.JCActor;
+
+/**
+ * Creates a JCActor.
+ */
+public class JCActorFactory extends ActorFactory {
+    /**
+     * The class of the root component.
+     */
+    private Class componentClass;
+
+    /**
+     * Create an ActorFactory.
+     *
+     * @param actorType      The actor type.
+     * @param componentClass The class of the root component.
+     */
+    public JCActorFactory(String actorType, Class componentClass) {
+        super(actorType);
+        this.componentClass = componentClass;
+    }
+
+    /**
+     * Create an actor.
+     *
+     * @param mailbox The mailbox of the new actor.
+     * @param parent  The parent of the new actor.
+     * @return The new actor.
+     */
+    @Override
+    public Actor newActor(Mailbox mailbox, Actor parent) throws Exception {
+        Include include = new Include(componentClass);
+        JCActor actor = new JCActor(mailbox);
+        actor.setActorType(actorType);
+        actor.setParent(parent);
+        include.call(actor);
+        return actor;
+    }
+}
