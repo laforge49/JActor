3cc8bc2f2471ad145e887b8c7cd237f209b680c2
Bill LaForge <laforge49@gmail.com>
2012-04-01 08:58:22 +0530

    new NBLockFactory

---
+++ src/main/java/org/agilewiki/jactor/nbLock/NBLockFactory.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2011 Bill La Forge
+ *
+ * This file is part of AgileWiki and is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License (LGPL) as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * or navigate to the following url http://www.gnu.org/licenses/lgpl-2.1.txt
+ *
+ * Note however that only Scala, Java and JavaScript files are being covered by LGPL.
+ * All other files are covered by the Common Public License (CPL).
+ * A copy of this license is also included and can be
+ * found as well at http://www.opensource.org/licenses/cpl1.0.txt
+ */
+package org.agilewiki.jactor.nbLock;
+
+import org.agilewiki.jactor.Mailbox;
+import org.agilewiki.jactor.factory.JLPCActorFactory;
+import org.agilewiki.jactor.lpc.JLPCActor;
+
+/**
+ * Creates a NBLock actor.
+ */
+public class NBLockFactory extends JLPCActorFactory {
+    /**
+     * The default type of the NBLock actor.
+     */
+    public final static String TYPE = "NBLock";
+
+    public NBLockFactory(String actorType) {
+        super(actorType);
+    }
+
+    @Override
+    protected JLPCActor instantiateActor(Mailbox mailbox) throws Exception {
+        return new NBLock(mailbox);
+    }
+}
