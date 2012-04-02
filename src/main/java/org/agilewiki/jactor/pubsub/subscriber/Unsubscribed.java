4d21ab30b1a596b7b9c70d429bcb01bcd0ebf0a7
Bill LaForge <laforge49@gmail.com>
2012-04-02 14:30:54 +0530

    new Unsubscribed request

---
+++ src/main/java/org/agilewiki/jactor/pubsub/subscriber/Unsubscribed.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2012 Bill La Forge
+ *
+ * This file is part of AgileWiki and is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License (LGPL) as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * or navigate to the following url http://www.gnu.org/licenses/lgpl-2.1.txt
+ *
+ * Note however that only Scala, Java and JavaScript files are being covered by LGPL.
+ * All other files are covered by the Common Public License (CPL).
+ * A copy of this license is also included and can be
+ * found as well at http://www.opensource.org/licenses/cpl1.0.txt
+ */
+package org.agilewiki.jactor.pubsub.subscriber;
+
+import org.agilewiki.jactor.Actor;
+import org.agilewiki.jactor.lpc.Request;
+import org.agilewiki.jactor.pubsub.publisher.Publisher;
+
+/**
+ * The target actor no longer has a subscription.
+ */
+final public class Unsubscribed extends Request<Object, Subscriber> {
+    /**
+     * The publisher to which the target actor was subscribed.
+     */
+    public final Publisher publisher;
+
+    /**
+     * Create a Unsubscribed request.
+     *
+     * @param publisher The publisher to which the target actor was subscribed.
+     */
+    public Unsubscribed(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    /**
+     * Returns true when targetActor is an instanceof TARGET_TYPE
+     *
+     * @param targetActor The actor to be called.
+     * @return True when targetActor is an instanceof TARGET_TYPE.
+     */
+    protected boolean isTargetType(Actor targetActor) {
+        return targetActor instanceof Subscriber;
+    }
+}
