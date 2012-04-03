7ab17bf76b50fe4de7a9de3576d0ea38001efbc0
Bill LaForge <laforge49@gmail.com>
2012-04-03 14:16:04 +0530

    dropped old ActorRegistry

--- src/main/java/org/agilewiki/jactor/components/actorRegistry/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Actors added to an ActorRegistry can be accessed by name
- * and are closed when the registry is closed.
- */
-package org.agilewiki.jactor.components.actorRegistry;
--- src/main/java/org/agilewiki/jactor/components/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * <p>
- *     JCActor is a final class which supports one type of request message, Include, which adds a Component to the
- *     actor. Each Component provides a list of other components that are to be included as well, unless they have
- *     already been included of course.
- * </p>
- */
-package org.agilewiki.jactor.components;
