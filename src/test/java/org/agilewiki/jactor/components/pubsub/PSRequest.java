3ccb57b2cac1d27f6d8c9d5e238f629e32a9bdec
Bill LaForge <laforge49@gmail.com>
2012-04-03 14:12:33 +0530

    dropped old PubSubTest

--- src/test/java/org/agilewiki/jactor/components/pubsub/PSRequest.java
+++
@@ -1,9 +0,0 @@
-package org.agilewiki.jactor.components.pubsub;
-
-import org.agilewiki.jactor.bind.AsyncRequest;
-
-/**
- * Test code.
- */
-final public class PSRequest extends AsyncRequest<Object> {
-}
