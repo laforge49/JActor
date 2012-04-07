591d935d3ecd4d29c639a35dc4efbd3ac6f1b531
Bill LaForge <laforge49@gmail.com>
2012-04-07 08:03:16 +0530

    cleanup

--- src/test/java/org/agilewiki/jactor/basics/Actor1.java
+++ src/test/java/org/agilewiki/jactor/basics/Actor1.java
@@ -5,11 +5,6 @@ import org.agilewiki.jactor.RP;
 import org.agilewiki.jactor.lpc.JLPCActor;
 
 public class Actor1 extends JLPCActor {
-    /**
-     * Create a LiteActor
-     *
-     * @param mailbox A mailbox which may be shared with other actors.
-     */
     public Actor1(final Mailbox mailbox) {
         super(mailbox);
     }
