bf91399f1cd5a27681dacb8aa47c3fbadc6c5b3b
Bill LaForge <laforge49@gmail.com>
2012-04-08 09:13:39 +0530

    minor cleanup

--- src/test/java/org/agilewiki/jactor/basics/Test4.java
+++ src/test/java/org/agilewiki/jactor/basics/Test4.java
@@ -10,12 +10,14 @@ public class Test4 extends TestCase {
     public void test() throws Exception {
         MailboxFactory mailboxFactory = JAMailboxFactory.newMailboxFactory(10);
         Mailbox mailbox = mailboxFactory.createMailbox();
+        JAFuture future = new JAFuture();
+
         Actor2 actor2 = new Actor2(mailbox);
         Actor4 actor4 = new Actor4(mailbox);
         actor4.setParent(actor2);
-        JAFuture future = new JAFuture();
         boolean result = Validate1.req.send(future, actor4);
         assertFalse(result);
+
         Actor1 actor1a = new Actor1(mailbox);
         Actor2 actor2a = new Actor2(mailbox);
         actor2a.setParent(actor1a);
